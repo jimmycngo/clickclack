727b5d0cc0ba1cc733df8fdc350134cb7ae28c04
jimmycngo <jimmycngo@gmail.com>
2021-06-22 11:05:00 -0700

    removed .env because i leaked secret

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/bundle.js'))
 })
 
-app.get('/index.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.js'))
-})
+// app.get('/index.js', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../src/index.js'))
+// })
 
-app.get('/index.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.css'))
-})
+// app.get('/index.css', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../src/index.css'))
+// })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
